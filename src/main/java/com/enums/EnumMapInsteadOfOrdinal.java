d8c4195a1fbdfa2f7e111c4e8a2660846fbdd6d0
Abhijeet Mohanty <mabhijeet1995@gmail.com>
2023-06-28 22:07:57 -0400

    Refactoring.

--- src/main/java/com/enums/EnumMapInsteadOfOrdinal.java
+++ src/main/java/com/enums/EnumMapInsteadOfOrdinal.java
@@ -1,6 +1,10 @@
 package com.enums;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class EnumMapInsteadOfOrdinal {
 
