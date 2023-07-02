92d83ea3f8e7408d17393d1a00bcb9c2acb5ee7b
Abhijeet Mohanty <mabhijeet1995@gmail.com>
2023-07-01 21:52:10 -0400

    Added "extensible" enum with the help of an interface.

---
+++ src/main/java/com/enums/MimicExtensibleEnumWithAnInterface.java
@@ -0,0 +1,91 @@
+package com.enums;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class MimicExtensibleEnumWithAnInterface {
+    public static void main(String[] args) {
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+
+        // pass class literal (bounded token type) to obtain all enum constants
+        testAllOpsWithBoundedTokenType(BasicOperation.class, x, y);
+        testAllOpsWithBoundedTokenType(ExtendedOperation.class, x, y);
+
+        // pass enum constants in a list
+        testAllOpsWithBoundedWildcardType(Arrays.asList(BasicOperation.values()), x, y);
+    }
+
+    // 1. "enumType" should represent both an enum and also a subtype of Operation
+    private static <T extends Enum<T> & Operation> void testAllOpsWithBoundedTokenType(Class<T> enumType, double x, double y) {
+        for (Operation op : enumType.getEnumConstants()) {
+            System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
+        }
+    }
+
+    private static void testAllOpsWithBoundedWildcardType(Collection<? extends Operation> ops, double x, double y) {
+        for (Operation op : ops) {
+            System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
+        }
+    }
+}
+
+interface Operation {
+    double apply(double x, double y);
+}
+
+enum BasicOperation implements Operation {
+
+    ADD("+") {
+        @Override
+        public double apply(double x, double y) {
+            return x + y;
+        }
+    }, MINUS("-") {
+        @Override
+        public double apply(double x, double y) {
+            return x - y;
+        }
+    }, MULTIPLY("*") {
+        @Override
+        public double apply(double x, double y) {
+            return x * y;
+        }
+    }, DIVIDE("/") {
+        @Override
+        public double apply(double x, double y) {
+            return x / y;
+        }
+    };
+
+    private final String opCode;
+
+    BasicOperation(String opCode) {
+        this.opCode = opCode;
+    }
+
+    @Override
+    public String toString() {
+        return opCode;
+    }
+}
+
+enum ExtendedOperation implements Operation {
+    REMAINDER("%") {
+        @Override
+        public double apply(double x, double y) {
+            return x % y;
+        }
+    };
+
+    private final String opCode;
+
+    ExtendedOperation(String opCode) {
+        this.opCode = opCode;
+    }
+
+    @Override
+    public String toString() {
+        return opCode;
+    }
+}
